ea29c3543e3cec56b9fa68097af2221eee8531b4
Cezary Sanecki <devcezz@gmail.com>
2020-10-25 18:11:45 +0100

    Add class for search params

---
+++ src/main/java/SearchParams.java
@@ -0,0 +1,10 @@
+public class SearchParams {
+
+    public final String job;
+    public final String city;
+
+    public SearchParams(String job, String city) {
+        this.job = job;
+        this.city = city;
+    }
+}
