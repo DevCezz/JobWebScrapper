331993ddc5c78bb0ce9cab7a5673089866a6ca83
Cezary Sanecki <devcezz@gmail.com>
2020-10-25 18:16:59 +0100

    Add class for web scrapping

---
+++ src/main/java/WebScrapper.java
@@ -0,0 +1,10 @@
+import com.gargoylesoftware.htmlunit.WebClient;
+
+public class WebScrapper {
+
+    private final WebClient webClient;
+
+    public WebScrapper(WebClient webClient) {
+        this.webClient = webClient;
+    }
+}
