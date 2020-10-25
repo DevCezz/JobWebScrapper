ed1833e2a633c50d3bffe108180f47e72d3b3614
Cezary Sanecki <devcezz@gmail.com>
2020-10-25 18:26:11 +0100

    Add constructor for km far

--- src/main/java/SearchParams.java
+++ src/main/java/SearchParams.java
@@ -4,9 +4,15 @@ public class SearchParams {
 
     public final String job;
     public final String city;
+    public final int kmFar;
 
-    public SearchParams(String job, String city) {
+    public SearchParams(String job, String city, int kmFar) {
         this.job = job;
         this.city = city;
+        this.kmFar = kmFar;
+    }
+
+    public SearchParams(String job, String city) {
+        this(job, city, DEFAULT_KM_FAR);
     }
 }
