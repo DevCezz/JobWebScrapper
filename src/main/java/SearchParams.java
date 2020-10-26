14ff9b27a891b1250db8981e988a7201b34d95ef
Cezary Sanecki <devcezz@gmail.com>
2020-10-26 18:01:14 +0100

    Delete far km param, is unnecessary

--- src/main/java/SearchParams.java
+++ src/main/java/SearchParams.java
@@ -1,18 +1,10 @@
 public class SearchParams {
 
-    private static final int DEFAULT_KM_FAR = 30;
-
     public final String job;
     public final String city;
-    public final int kmFar;
-
-    public SearchParams(String job, String city, int kmFar) {
-        this.job = job;
-        this.city = city;
-        this.kmFar = kmFar;
-    }
 
     public SearchParams(String job, String city) {
-        this(job, city, DEFAULT_KM_FAR);
+        this.job = job;
+        this.city = city;
     }
 }
