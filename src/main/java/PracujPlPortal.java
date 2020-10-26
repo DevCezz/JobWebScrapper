f0563ea65346c033dfd30381a81f821b69ae2896
Cezary Sanecki <devcezz@gmail.com>
2020-10-27 00:34:13 +0100

    Make PracujPlPortal implementing properly PortalStrategy

--- src/main/java/PracujPlPortal.java
+++ src/main/java/PracujPlPortal.java
@@ -1,19 +1,15 @@
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
 public class PracujPlPortal implements PortalStrategy {
 
-    private final static String URL_PAGE = "https://www.pracuj.pl/praca/%s;kw/%s;wp?rd=%d";
+    private final static String URL_PAGE = "https://www.pracuj.pl/praca/%s;kw/%s;wp?rd=10";
+    private final static String CSS_SELECTOR = "#results .offer-details__title-link";
 
-    private final WebScrapingClient client;
-
-    public PracujPlPortal(WebScrapingClient client) {
-        this.client = client;
-        this.client.disableCss();
-        this.client.disableJs();
+    @Override
+    public String createPageUrl(SearchParams params) {
+        return String.format(URL_PAGE, params.job, params.city);
     }
 
     @Override
-    public void foo(SearchParams params) {
-        HtmlPage page = client.scrape(URL_PAGE);
+    public String cssSelectorToLinkOffers() {
+        return CSS_SELECTOR;
     }
 }
