3939a8e9d51684f47166ee008db465a5ef47ac04
Cezary Sanecki <devcezz@gmail.com>
2020-10-26 18:04:03 +0100

    Add own client for webscraping

---
+++ src/main/java/WebScrapingClient.java
@@ -0,0 +1,10 @@
+import com.gargoylesoftware.htmlunit.WebClient;
+
+public class WebScrapingClient {
+
+    private final WebClient webClient;
+
+    public WebScrapingClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+}
