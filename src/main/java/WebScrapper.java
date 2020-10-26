515af3955174e5a82c8a47e644c02747bf451605
Cezary Sanecki <devcezz@gmail.com>
2020-10-27 00:35:12 +0100

    Make WebScrapper to be main invoker

--- src/main/java/WebScrapper.java
+++ src/main/java/WebScrapper.java
@@ -1,39 +1,13 @@
 import com.gargoylesoftware.htmlunit.WebClient;
 
-import java.io.IOException;
-
 public class WebScrapper {
 
-    private final static String urlPage = "https://www.pracuj.pl/praca/%s;kw/%s;wp?rd=%d";
+    public static void main(String[] args) {
+        WebClient webClient = new WebClient();
+        PortalStrategy portalStrategy = new PracujPlPortal();
+        WebScrapingClient client = new WebScrapingClient(webClient, portalStrategy);
 
-    private PortalStrategy portalStrategy;
-
-    public WebScrapper(PortalStrategy portalStrategy) {
-        this.portalStrategy = portalStrategy;
-    }
-
-    public void scrape(SearchParams params) {
-        try {
-            String urlWithParams = String.format(urlPage, params.job, params.city, params.kmFar);
-            webClient.getPage(urlWithParams);
-        } catch (IOException e) {
-            throw new CannotReachPageException("Cannot connect to " + urlPage);
-        }
-    }
-
-    public void disableCss() {
-        webClient.getOptions().setCssEnabled(false);
-    }
-
-    public void enableCss() {
-        webClient.getOptions().setCssEnabled(true);
-    }
-
-    public void disableJavasript() {
-        webClient.getOptions().setJavaScriptEnabled(false);
-    }
-
-    public void enableJavasript() {
-        webClient.getOptions().setJavaScriptEnabled(true);
+        client.turnOffLogs();
+        client.scrape(new SearchParams("Java", "Warszawa"));
     }
 }
