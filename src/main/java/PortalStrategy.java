c4d3735ebcc9b9a13fda3cf270c959bc815921f6
Cezary Sanecki <devcezz@gmail.com>
2020-10-28 01:18:50 +0100

    Extend PortalStrategy interface

--- src/main/java/PortalStrategy.java
+++ src/main/java/PortalStrategy.java
@@ -6,7 +6,11 @@ public interface PortalStrategy {
 
     String createPageUrl(SearchParams params, int pageNum);
 
+    String cssSelectorForPagination();
+
     String cssSelectorToLinkOffers();
 
     JobPosition assembleJobFrom(HtmlPage page);
+
+    String createAbsolutePath(String path);
 }
